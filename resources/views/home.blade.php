b60c5f2b332b9c84d94bc4c530051d83868968b2
Kamilplhh <Kamil_PLHH@wp.pl>
2023-04-20 01:17:58 +0200

    parallax effect added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,25 @@
 
 @section('content')
 <div class="landing">
-    <iframe id="iframe" height="100%" width="100%" frameborder="0" autoplay 
-        src="https://www.youtube.com/embed/31kplxJn6nw?autoplay=1">
-    </iframe>
-
+    <video id="video" height="100%" width="100%" frameborder="0" loop autoplay muted
+        src="images/background.mp4">
+    <video>
 </div>
 <div class="landingOverlay">
     <h3>Welcome to E-cars where u can find szybkie i dobre electronic cars</h3>
     <br>
     <h4>Created by KamilPLHH</h4>
 </div>
+
+<div class="div">
+    <div class="parallax">
+        <div class="parallax-1" id="1">
+        </div>
+    </div>  
+    <div class="parallax-2">
+        <div class="parallax-1" id="1">
+        </div>
+    </div>  
+</div>
+
 @stop
